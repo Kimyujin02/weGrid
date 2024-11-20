00a458a1b9a7bf5274b39c49d5fa43f34103a590
yu <habveluit01345@gmail.com>
2024-11-20 14:35:01 +0900

    초기화

---
+++ wegrid/src/main/java/com/kh/wegrid/WegridApplication.java
@@ -0,0 +1,13 @@
+package com.kh.wegrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WegridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WegridApplication.class, args);
+	}
+
+}
---
+++ wegrid/src/main/java/com/kh/wegrid/home/controller/HomeContoller.java
@@ -0,0 +1,15 @@
+package com.kh.wegrid.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("home")
+public class HomeContoller {
+
+    @GetMapping
+    public String home(){
+        return "home";
+    }
+}
