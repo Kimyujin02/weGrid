6338aaaff17733a87f35827302999925676eb061
yu <habveluit01345@gmail.com>
2024-12-03 09:02:53 +0900

    crm 수정

---
+++ wegrid/src/main/java/com/kh/wegrid/crm/service/CrmService.java
@@ -0,0 +1,56 @@
+package com.kh.wegrid.crm.service;
+
+import com.kh.wegrid.crm.mapper.CrmMapper;
+import com.kh.wegrid.crm.vo.ClientHistoryVo;
+import com.kh.wegrid.crm.vo.ClientRankVo;
+import com.kh.wegrid.crm.vo.ClientStatusVo;
+import com.kh.wegrid.crm.vo.ClientVo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional
+public class CrmService {
+
+    private final CrmMapper mapper;
+
+    public List<ClientVo> getClientVoList() {
+        return mapper.getClientVoList();
+    }
+
+    public List<ClientRankVo> getClientRankVoList() {
+        return mapper.getClientRankVoList();
+    }
+
+    public List<ClientStatusVo> getClientStatusVoList() {
+        return mapper.getClientStatusVoList();
+    }
+
+    public List<ClientVo> getClientVoListData(String searchType, String searchValue) {
+        return mapper.getClientVoListData(searchType, searchValue);
+    }
+
+    // 상세 조회
+    public ClientVo getDetail(String cno) {
+        return mapper.getDetail(cno);
+    }
+
+    // 상세 조회 (프로젝트 데이터)
+    public List<ClientVo> getPrjVoList(String cno, String searchValue) {
+        return mapper.getPrjVoList(cno, searchValue);
+    }
+
+    public int enroll(ClientVo vo) {
+        return mapper.enroll(vo);
+    }
+
+    public List<ClientHistoryVo> getHistoryVoList(String cno, String searchValue) {
+        return mapper.getHistoryVoList(cno, searchValue);
+    }
+}
