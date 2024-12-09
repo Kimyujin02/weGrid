167b7fdceb2e040bb19641c099b165e4e8b088db
YUJIN\yujin <habveluit01345@gmail.com>
2024-12-09 17:57:23 +0900

    관리자 로그인으로 넘어가는 로직 추가

--- wegrid/src/main/java/com/kh/wegrid/systemManager/controller/SystemManagerController.java
+++ wegrid/src/main/java/com/kh/wegrid/systemManager/controller/SystemManagerController.java
@@ -2,6 +2,7 @@ package com.kh.wegrid.systemManager.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kh.wegrid.member.vo.AdminVo;
 import com.kh.wegrid.member.vo.MemberVo;
 import com.kh.wegrid.systemManager.service.SystemManagerService;
 import com.kh.wegrid.systemManager.vo.DepartMentVo;
@@ -71,11 +72,11 @@ public class SystemManagerController {
             RedirectAttributes redirectAttributes
     ) {
 
-        MemberVo loginMemberVo = (MemberVo) session.getAttribute("loginMemberVo");
         // 로그인된 사용자가 없을 때 처리
-        if (loginMemberVo == null) {
-            redirectAttributes.addFlashAttribute("errorMessage", "로그인 정보가 없습니다.");
-            return "redirect:/member/login"; // 로그인 페이지로 리다이렉트
+        AdminVo loginAdminVo = (AdminVo) session.getAttribute("loginAdminVo");
+        if(loginAdminVo == null){
+            session.setAttribute("alertMsg","옳바르지 않은 접근 입니다. 관리자 로그인화면으로 이동합니다.");
+            return "redirect:/member/adminLogin";
         }
 
         // 1. 부서 및 직급 리스트 가져오기
